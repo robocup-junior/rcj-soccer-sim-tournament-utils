b396c9b3cf4b83d6ac7c5b8d680ecb06e4270a9c
David Schwarz <rcj-soccer-sim-feb-21@davidschwarz.info>
2021-02-21 14:07:53 +0100

    Tournament data for rcj-soccer-sim-2021-feb competition and bugfix in-tournament changes

--- get_score.py
+++ get_score.py
@@ -2,9 +2,10 @@
 import json, sys, os
 rec_dir = sys.argv[1]
 rec_dir_files = os.listdir(rec_dir)
+previous_half_ident = '_-_0_-_' if len(sys.argv) == 4 else sys.argv[4]
 for rec_dir_file in rec_dir_files:
   match_id = sys.argv[2]
-  if match_id in rec_dir_file and '_-_0_-_' in rec_dir_file and '.jsonl' in rec_dir_file:
+  if match_id in rec_dir_file and previous_half_ident in rec_dir_file and '.jsonl' in rec_dir_file:
     fd = open(rec_dir + os.sep + rec_dir_file, 'r')
     lines = fd.readlines()
     line = lines[-1]
